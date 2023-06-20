bc117c1fb8b8fe8f1fb7d4cd4c16ac91c980431a
Jianzhang Ni <jzni132134@gmail.com>
2023-06-20 13:50:31 +0800

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -2,7 +2,7 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: '1b508d733ce440698a20f31b33656eac',
+  rootNotionPageId: '87c714430e8041c2b02421b93ef1eb0e',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
@@ -14,7 +14,7 @@ export default siteConfig({
   author: 'Ni',
 
   // open graph metadata (optional)
-  description: 'Example Next.js Notion Starter Kit Site',
+  description: 'This is Neil's Blog Page',
 
   // social usernames (optional)
   twitter: 'Jianzha23261569',
