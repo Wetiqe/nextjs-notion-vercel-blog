bd7fdca674a6bceb2c8b2d243f768f7c05a64d37
Jianzhang Ni <jzni132134@gmail.com>
2023-06-20 13:50:59 +0800

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -14,7 +14,7 @@ export default siteConfig({
   author: 'Ni',
 
   // open graph metadata (optional)
-  description: 'Example Next.js Notion Starter Kit Site',
+  description: 'This is Neil's Homepage',
 
   // social usernames (optional)
   twitter: 'Jianzha23261569',
